e6728f0b873d97bb7f129d0a176b1f6007322672
srujanGowda08 <srujangamer08@gmail.com>
2024-04-24 12:30:19 +0530

    smallchanges

--- 5-keyboard/chaiaurcode.js
+++ 5-keyboard/chaiaurcode.js
@@ -1,21 +1,20 @@
 const insert = document.getElementById("insert");
 
 window.addEventListener("keydown", (e) => {
-  insert.innerHTML = `
+    insert.innerHTML = `
     <div class='color'>
     <table>
     <tr>
-      <th>Key</th>
-      <th>Keycode</th> 
-      <th>Code</th>
+        <th>Key</th>
+        <th>Keycode</th> 
+        <th>Code</th>
     </tr>
     <tr>
-      <td>${e.key === " " ? "Space" : e.key}</td>
-      <td>${e.keyCode}</td> 
-      <td>${e.code}</td>
+        <td>${e.key === " " ? "Space" : e.key}</td>
+        <td>${e.keyCode}</td> 
+        <td>${e.code}</td>
     </tr>
     
-  </table>
-    </div>
-  `;
+    </table>
+    </div>`;
 });
